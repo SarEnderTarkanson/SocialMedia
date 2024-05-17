86caf705ad6f2299d440497aa49414a5bc8e74dd
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-17 03:04:42 +0200

    Title component and its style created

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import {getFontFamily} from './assets/fonts/helper';
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '100')}}>
-        Hello World!
-      </Text>
+      <Title title={"Let's Explore"} />
     </SafeAreaView>
   );
 };
