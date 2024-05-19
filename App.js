b78c42f9faf24fdce13254d4a0ad31e015ac977a
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-19 02:38:01 +0200

    added the UserStory component

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import {SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import globalStyle from './assets/styles/globalStyle';
+import UserStory from './components/UserStory/UserStory';
 
 const App = () => {
   const userStories = [
@@ -57,7 +64,6 @@ const App = () => {
       id: 1,
       profileImage: require('./assets/images/default_profile.png'),
     },
-
   ];
   return (
     <SafeAreaView>
@@ -69,6 +75,19 @@ const App = () => {
             <Text style={globalStyle.messageNumber}>2</Text>
           </View>
         </TouchableOpacity>
+      </View>
+      <View style={globalStyle.userStoryContainer}>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          data={userStories}
+          renderItem={({item}) => (
+            <UserStory
+              firstName={item.firstName}
+              profileImage={item.profileImage}
+            />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
