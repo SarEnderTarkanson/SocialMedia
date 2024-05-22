852a21e21772f6526ebbe7ffa4833a4ff486936d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-22 02:46:45 +0200

    Practice on Dimensions API

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  Dimensions,
 } from 'react-native';
 import Title from './components/Title/Title';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
@@ -134,6 +135,9 @@ const App = () => {
   const [userPostsRenderedData, setUserPostsRenderedData] = useState([]);
   const [isLoadingUserPosts, setIsLoadingUserPosts] = useState(false);
 
+  const [screenData, setScreenData] = useState(Dimensions.get('screen'));
+
+  console.log(screenData);
   const pagination = (database, currentPage, pageSize) => {
     const startIndex = (currentPage - 1) * pageSize;
     const endIndex = startIndex + pageSize;
@@ -153,10 +157,24 @@ const App = () => {
     const getInitialDataPosts = pagination(userPosts, 1, userPostsPageSize);
     setUserPostsRenderedData(getInitialDataPosts);
     setIsLoadingUserPosts(false);
+
+    Dimensions.addEventListener('change', result => {
+      setScreenData(result.screen);
+    });
   }, []);
 
   return (
     <SafeAreaView>
+      <View
+        style={{
+          backgroundColor: 'red',
+          width: screenData.width / 2,
+          height: screenData.height / 2,
+        }}>
+        <Text style={{fontSize: screenData.height / 20}}>
+          This Box will have half of the screen's width and height
+        </Text>
+      </View>
       <View>
         <FlatList
           ListHeaderComponent={
