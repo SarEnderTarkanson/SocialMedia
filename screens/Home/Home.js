0f0df405ec6cc99a87ec2020ce9a24e18b2c5d20
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-25 02:16:09 +0200

    added a drawer navigator

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -168,11 +168,7 @@ const Home = ({navigation}) => {
             <>
               <View style={style.header}>
                 <Title title={"Let's Explore"} />
-                <TouchableOpacity
-                  style={style.messageIcon}
-                  onPress={() => {
-                    navigation.navigate(Routes.Profile);
-                  }}>
+                <TouchableOpacity style={style.messageIcon}>
                   <FontAwesomeIcon
                     icon={faEnvelope}
                     size={scaleFontSize(20)}
