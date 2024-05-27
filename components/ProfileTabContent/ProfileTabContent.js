3672c8aa2be5e34191517ae458ce47a8c26fc94d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-27 02:57:39 +0200

    added ProfileTabContent

---
+++ components/ProfileTabContent/ProfileTabContent.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import style from './style';
+import {Image, ScrollView, View} from 'react-native';
+
+const ProfileTabContent = () => {
+  return (
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={style.profileTabContentContainer}>
+      <View style={style.profileTabContent}>
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+        <Image
+          resizeMode="contain"
+          style={style.imageStyle}
+          source={require('../../assets/images/default_post.png')}
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default ProfileTabContent;
